de381d43ef41b956998d321c9dfd4508f9d450f3
Zlatko Verk <zlatko112@gmail.com>
2026-03-18 11:49:13 +0100

    [Connection] Ltrim newline from stream_get_line

--- src/Connection.php
+++ src/Connection.php
@@ -61,7 +61,7 @@ class Connection
 
         while (true) {
             $message = null;
-            $line = stream_get_line($this->socket, 1024, "\r\n");
+            $line = ltrim(stream_get_line($this->socket, 1024, "\r\n"));
             $now = microtime(true);
             if ($line) {
                 $message = Factory::create($line);
